54ad6c1269127d912ead4a926ceff83069d81651
devSuryansh <suryanshg2050@gmail.com>
2025-01-08 18:38:16 +0530

    Deletes the file

--- client/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as FeaturedHotels } from "./featured-hotels";
-export { default as Footer } from "./footer";
-export { default as Header } from "./header";
-export { default as Hero } from "./hero";
-// export { ImageGalleryModal } from "./image-gallery-modal";
