1bec9d9e473d0fdf257b63522a8ad106d22b0220
devSuryansh <suryanshg2050@gmail.com>
2025-01-08 18:00:37 +0530

    Cleans the junk

--- frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import FeaturedHotels from "./FeaturedHotels";
-import Footer from "./Footer";
-import Header from "./Header";
-import Hero from "./Hero";
-
-export { FeaturedHotels, Footer, Header, Hero };
