980d6672af2e4e6a51eb5839d1aea3706d99055d
devSuryansh <suryanshg2050@gmail.com>
2025-02-23 15:02:49 +0530

    Sets footer component to render in each page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,11 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedHotels from "@/components/FeaturedHotels";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <FeaturedHotels />
-      <Footer />
     </main>
   );
 }
